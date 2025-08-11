5c5b090e0b291f38d484aa17fb4a4b7e975e0cda
bonniesimmer <noura19918@gmail.com>
2025-08-11 19:29:34 +0300

    Added AssertJ dependency and changed AccountManagerTest format to be easily extensible.

--- src/test/java/example/account/AccountManagerTest.java
+++ src/test/java/example/account/AccountManagerTest.java
@@ -1,23 +1,36 @@
 package example.account;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class AccountManagerTest {
+    private AccountManager accountManager;
+    private Customer customer;
 
-    @Test
-    void givenCustomerWithSufficientBalance_whenWithdraw_thenSucceed() {
-        // Arrange
-        AccountManager am = new AccountManagerImpl();
-        Customer c = new Customer();
-        c.setBalance(1000);
-
-        // Act
-        String result = am.withdraw(c, 500);
-
-        // Assert
-        Assertions.assertEquals("success", result);
-        Assertions.assertEquals(500, c.getBalance());
+    @BeforeEach
+    void setUp() {
+        accountManager = new AccountManagerImpl();
+        customer = new Customer();
     }
+
+    @Nested
+    class WithdrawTests {
+        @Test
+        void givenCustomerWithSufficientBalance_whenWithdraw_thenSucceed() {
+            // Arrange
+            customer.setBalance(1000);
+
+            // Act
+            String result = accountManager.withdraw(customer, 500);
+
+            // Assert
+            assertThat(result).isEqualTo("success");
+            assertThat(customer.getBalance()).isEqualTo(500);
+        }
+    }
+
 
 }
