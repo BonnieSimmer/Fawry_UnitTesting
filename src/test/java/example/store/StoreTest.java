664a24d61f622b2eba38bd3ab6195b45f10066fb
bonniesimmer <noura19918@gmail.com>
2025-08-11 19:54:33 +0300

    Changed StoreTest to be extensible and removed unneeded files

--- src/test/java/example/store/StoreTest.java
+++ src/test/java/example/store/StoreTest.java
@@ -2,38 +2,38 @@ package example.store;
 
 import example.account.AccountManager;
 import example.account.Customer;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 public class StoreTest {
+    private AccountManager accountManager;
+    private Store store;
+    private Product product;
+    private Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        accountManager = mock(AccountManager.class);
+        store = new StoreImpl(accountManager);
+        product = new Product();
+        customer = new Customer();
+    }
 
     @Test
-    void test() {
+    void givenEnoughQuantityAndSuccessfulWithdraw_WhenBuy_ThenQuantityDecrease() {
         // Arrange
-        AccountManager accountManager = new AccountManagerStub();
-        Store store = new StoreImpl(accountManager);
-        Product product = new Product();
+        when(accountManager.withdraw(any(), anyInt())).thenReturn("success");
         product.setQuantity(4);
-        Customer customer = new Customer();
 
         // Act
         store.buy(product, customer);
 
         // Assert
-        Assertions.assertEquals(3, product.getQuantity());
-    }
-
-    static class AccountManagerStub implements AccountManager {
-
-        @Override
-        public void deposit(Customer customer, int amount) {
-
-        }
-
-        @Override
-        public String withdraw(Customer customer, int amount) {
-            return "success";
-        }
+        assertThat(product.getQuantity()).isEqualTo(3);
+        verify(accountManager, times(1)).withdraw(customer, product.getPrice());
     }
 
 }
